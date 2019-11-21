cabf3d788fe34393817d92daad5fcd036231ce8a
Jairo Burgos Guarin <jyburgos40@gmail.com>
2019-11-20 21:46:44 -0500

    Routing and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { app_routes } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { RankingComponent } from './components/ranking/ranking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeroComponent,
+    RankingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routes
   ],
   providers: [],
   bootstrap: [AppComponent]
